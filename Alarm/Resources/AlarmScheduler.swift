092e0fa67ccd9a951d7565156d5eef79fad57670
Will Morris <william.morris117@gmail.com>
2019-05-07 14:57:08 -0600

    Added User notifications and persistance

---
+++ Alarm/Resources/AlarmScheduler.swift
@@ -0,0 +1,41 @@
+//
+//  AlarmScheduler.swift
+//  Alarm
+//
+//  Created by Will morris on 5/7/19.
+//  Copyright © 2019 devmtn. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+protocol AlarmScheduler: class {
+    
+    func scheduleUserNotifications(for alarm: Alarm)
+    func cancelUserNotifications(for alarm: Alarm)
+    
+}
+
+extension AlarmScheduler {
+    
+    func scheduleUserNotifications(for alarm: Alarm) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Alarm"
+        content.body = "Alarm is going off"
+        content.sound = .default
+        let dateComponents = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: alarm.fireDate)
+        let dateTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: alarm.uuid, content: content, trigger: dateTrigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("There was an error processing request: \(error) : \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func cancelUserNotifications(for alarm: Alarm) {
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [alarm.uuid])
+    }
+    
+}
